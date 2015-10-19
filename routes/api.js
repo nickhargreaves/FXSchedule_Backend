674ea0abce5f2454c54ea0c36bc19e6f8d2a58ed
Nick Hargreaves <hargreaves1234@gmail.com>
2015-10-19 07:15:31 +0300

    add CRUD placeholder apis for post

--- routes/api.js
+++ routes/api.js
@@ -16,4 +16,27 @@ router.route('/posts')
         res.send({message: 'TODO Create a new post'})
     });
 
+router.route('posts/:id')
+
+    //return specified post
+    .get(function(req, res){
+
+        res.send({message: 'return post with id' + req.params.id})
+
+    })
+
+    //update existing post
+    .put(function(req, res){
+
+        res.send({message: 'TODO modify post with id' + req.params.id})
+
+    })
+
+    //update existing post
+    .delete(function(req, res){
+
+        res.send({message: 'TODO delete post with id' + req.params.id})
+
+    })
+
 module.exports = router;
